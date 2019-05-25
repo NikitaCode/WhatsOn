fc119770b999bc5ee3d781354b011fbd6f22add1
Nikita <nikitush10@gmail.com>
2019-05-26 09:46:39 +1000

    collection view and firebase implamentation

---
+++ WhatsOn/WhatsOn/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  WhatsOn
+//
+//  Created by Nikita Sherbakov on 25/5/19.
+//  Copyright © 2019 Nikita Sherbakov. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellLabel: UILabel!
+    
+}
--- WhatsOn/WhatsOn/ViewController.swift
+++ WhatsOn/WhatsOn/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let items = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
 
+        cell.cellLabel.text = items[indexPath.item]
+
+        return cell
+    }
+    
+
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
 
 }
 
